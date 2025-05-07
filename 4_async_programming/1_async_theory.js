73ae747b51f449ed8da6982afd2aa5e894ee3e4c
seorang42 <seorang42@naver.com>
2025-05-07 22:38:03 +0900

    4-1 비동기 프로그래밍 소개

---
+++ 4_async_programming/1_async_theory.js
@@ -0,0 +1,34 @@
+// Async theory
+
+// Sync Programming
+// function longWork() {
+//   const now = new Date();
+
+//   /**
+//    * milliseconds since epoch
+//    * 1970년도 1월 1일부터 지금 코드가 실행되는 순간까지의
+//    * 시간을 밀리초로 반환한다.
+//    */
+//   const milliseconds = now.getTime();
+//   const afterTwoSeconds = milliseconds + 2 * 1000;
+
+//   while (new Date().getTime() < afterTwoSeconds) {}
+
+//   console.log("완료");
+// }
+
+// console.log("Hello");
+// longWork();
+// console.log("World"); // 2초 뒤에 실행
+
+// Async Programming
+function longWork() {
+  setTimeout(() => {
+    console.log("완료");
+  }, 2000);
+}
+
+console.log("Hello");
+longWork();
+console.log("World");
+// Hello -> World -> 완료
